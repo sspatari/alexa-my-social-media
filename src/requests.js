b0e5cf35a2f2f579b2074ae41262c0d4cc14daf4
sspatari <spatari.stanislav@gmail.com>
2018-04-28 11:29:06 +0200

    Implement GetLastSocialMediaUpdateIntentHandler

--- src/requests.js
+++ src/requests.js
@@ -2,17 +2,22 @@ import request from 'request';
 
 class Requests {
     constructor() {
-    
+
     }
 
     redditTIL() {
-        let url = 'https://www.reddit.com/r/todayilearned/.json';
+      let url = 'https://www.reddit.com/r/todayilearned/.json';
+      return new Promise((resolve, reject) => {
         request.get(url, (err, res, body) => {
+          if(err)
+            reject(`GET failed, ${err}`);
+          else {
             let children = JSON.parse(body).data.children;
             let firstPostTitle = children[0].data.title.slice(4);
-
-            return firstPostTitle;
+            resolve(firstPostTitle);
+          }
         });
+      });
     }
 }
 
