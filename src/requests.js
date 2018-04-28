95f70a3f584fd0817190ebac88c9b1e5effc24d3
strdr4605 <str.dr4605@gmail.com>
2018-04-28 15:51:31 +0200

    add pre text

--- src/requests.js
+++ src/requests.js
@@ -10,7 +10,7 @@ class Requests {
         let url = 'https://www.reddit.com/r/todayilearned/.json';
         request.get(url, (err, res, body) => {
             let children = JSON.parse(body).data.children;
-            let firstPostTitle = children[0].data.title.slice(4);
+            let firstPostTitle = 'Today I learned' + children[0].data.title.slice(4);
 
             return firstPostTitle;
         });
